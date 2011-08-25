205db10b71e86995b02871d195476afc408ec9cf
Mark D. Thompson <thomesoni@gmail.com>
2011-08-24 21:13:23 -0400

    added documentation and swapped out my read key

--- uclassify.php
+++ uclassify.php
@@ -1,9 +1,22 @@
 <?php
+/**
+* This is the main uClassify php script
+*
+* It uses the uClassify URL RESTish API 
+* see http://uclassify.com/UrlApiDocumentation.aspx 
+*
+* Be sure to sign up with uClassify to get your read key 
+* and replace 'YOUR_READ_API_KEY_HERE' with your own read key.
+*
+* @author Mark Thompson <mark@smithandthompson.net>
+*
+**/
+
 $cmd = $_GET['cmd'];
 $url = $_GET['url'];
 
 $baseUrl = 'http://uclassify.com/browse/uClassify/';
-$readkey = 'vhSVEuFSAZXrYxkCj3oxXN0TI+E=';
+$readkey = 'YOUR_READ_API_KEY_HERE'; // <- insert your key here
 $removeHTML = 1;
 $encoding = 'xml';
 $data = '';
@@ -39,3 +52,4 @@ function classify($qs){
   return $data;
 }
 ?>
+vhSVEuFSAZXrYxkCj3oxXN0TI+E=
