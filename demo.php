54d31f64ff4c9fada02505ad6e48c17047caf8e8
Mark D. Thompson <thomesoni@gmail.com>
2011-08-29 12:03:43 -0400

    edits

--- demo.php
+++ demo.php
@@ -1,105 +1,70 @@
 <?php
-/**
-* This a lightweight uClassify RESTish API php wrapper class
-*
-* Encapsulates a subset of the uClassify URL RESTish API 
-* see http://uclassify.com/UrlApiDocumentation.aspx 
-*
-* Be sure to sign up with uClassify to get your read key 
-* and replace 'YOUR_READ_API_KEY_HERE' with your own read key.
-*
-* @author Mark Thompson <mark@smithandthompson.net>
-*
-**/
-
-class uClassify {
-	
-	// private properties
-	private $baseUrl = 'http://uclassify.com/browse/uClassify/';
-	private $readkey = 'vhSVEuFSAZXrYxkCj3oxXN0TI+E='; // <- your read key here
-	private $removeHTML = 1;
-	private $encoding = 'json';
-	private $classifier = "topics"; // default to topics command
-	private $classifier_whitelist = array("topics");
-	
-	// Class methods
-    public function __construct()  {  
-    	//echo 'The class "', __CLASS__, '" was initiated!<br />';
-    }  
-	
-    public function __destruct() { 
-    	//echo 'The class "', __CLASS__, '" was destroyed.<br />';
-    }  
-    
-    public function __toString() {
-    	return implode(', ', get_object_vars($this));
-    }
-	
-    // Property accessors
-    
-    // set removeHTML
-	public function setRemoveHTML($trueOrFalse) {
-		if($trueOrFalse){
-			$this->removeHTML = 1;
-		} else {
-			$this->removeHTML = 0;
-		}
-		return $this->removeHTML;
-	}
-	
-	// get removeHTML
-	public function getRemoveHTML() {
-		return $this->removeHTML;
-	}
-	
-	// set encoding
-	public function setEncoding($newEncoding) {
-		if($newEncoding == 'xml' || $newEncoding == 'json'){
-			$this->removeHTML = $newEncoding;
-			return 0;
-		} else {
-			return -1;
-		}
-	}
-	
-	// get encoding
-	public function getEncoding() {
-		return $this->Encoding;
-	}
-	
-	//set the classifier
-	public function setClassifier($c){
-		if(in_array($c,$this->classifier_whitelist)){
-			$this->classifier = $c;
-			return 0;
-		} else {
-			return -1;
-		}
-	}
-	
-	public function getClassifier(){
-		return $this->classifier;	
-	}
-	
-	// Public methods
-	public function classifyUrl($url) {
-		// assemble the query string
-		$qs = $this->baseUrl.ucfirst($this->classifier).'/ClassifyUrl/?readkey='.urlencode($this->readkey).
-				'&url='.urlencode($url).'&removeHtml='.$this->removeHTML.'&output='.$this->encoding;
-		
-		// instantiate cUrl
-		$curl = curl_init($qs);
-		
-		// set options
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_HEADER, 0);
-		
-		// execute, close & return result
-		$data = curl_exec($curl);
-		
-		curl_close($curl);
-		
-		return $data;
-	}
+function __autoload($class_name) {
+    include $class_name . '.php';
 }
+
+// which classifier are we using (currently only accepts 'topics'
+$classifier = 'topics';
+// initialize json variable to empty for error checking
+$json = '';
+
+$uc = new uClassify();
+$err = $uc->setClassifier($classifier);
+
+$url = "http://www.nytimes.com/2011/08/29/us/29hurricane.html?partner=rss&emc=rss";
+if(!$err){
+	$json = $uc->classifyUrl($url);
+}
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	
+	<title>Classification Results for <?php echo $url; ?></title>
+	
+	<!-- meta tags -->
+	<meta name="keywords" content="uClassify, classifier, classification, topics, text">
+	<meta name="description" content="uCLassify classification results for user supplied uri">
+	
+	<!-- javascript -->
+	<script src="./scripts/raphael-min.js"></script>
+	<script src="./scripts/g.raphael-min.js"></script>
+	<script src="./scripts/g.pie-min.js"></script>
+	
+</head>
+<body>
+	<header id="main">
+		<h1>uClassify Classifier Results</h1>
+	</header>
+	
+	<section id="content">
+	
+	<?php if(!empty($json)):?>
+	
+	<?php echo ucfirst($classifier); ?> classification for: <a href="<?php echo $url; ?>"><?php echo $url; ?></a>
+	
+	<script>
+		var data = <?php echo $json; ?>;
+
+		// Creates canvas 640 × 480 at 0, 0
+		var r = Raphael(0, 0, 640, 480);
+		// Creates pie chart at with center at 320, 200,
+		// radius 100 and data from the classification
+		
+		var set = new Array();
+		var legend = new Array();
+		var i = 0;
+		for(var key in data.cls1){
+			legend[i] = key+': '+Math.round((data.cls1[key]*100)*100)/100+'%';
+			set[i] = data.cls1[key];
+			i++;
+		}
+		
+		r.g.piechart(240, 240, 100, set, {legend: legend, legendpos: "west"});
+	</script>
+	<?php endif; ?>
+	</section>
+</body>
+</html>
