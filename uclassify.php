c4a4c846aa0cd0417c2cd995d3ef51e02e5212f3
Mark D. Thompson <thomesoni@gmail.com>
2011-08-29 08:57:58 -0400

    converted script to uClassify API wrapper class

--- uclassify.php
+++ uclassify.php
@@ -1,8 +1,8 @@
 <?php
 /**
-* This is the main uClassify php script
+* This a lightweight uClassify RESTish API php wrapper class
 *
-* It uses the uClassify URL RESTish API 
+* Encapsulates a subset of the uClassify URL RESTish API 
 * see http://uclassify.com/UrlApiDocumentation.aspx 
 *
 * Be sure to sign up with uClassify to get your read key 
@@ -12,43 +12,79 @@
 *
 **/
 
-$cmd = $_GET['cmd'];
-$url = $_GET['url'];
-
-$baseUrl = 'http://uclassify.com/browse/uClassify/';
-$readkey = 'YOUR_READ_API_KEY_HERE'; // <- your read key here
-$removeHTML = 1;
-$encoding = 'xml';
-$data = '';
-
-if($encoding == 'xml'){
-  header ("content-type: text/xml");
-} else if($encoding == 'json'){
-  header ("content-type: text/javascript");
-}
-
-switch($cmd){
-  case 'topics':
-    // call search-fields to set up search-field-values menus
-    $qs = $baseUrl.'Topics'.'/ClassifyUrl/?readkey='.urlencode($readkey).'&url='.urlencode($url).'&removeHtml='.$removeHTML.'&output='.$encoding;
-    $data = classify($qs);
-    echo $data;
-  break;
-}
-
-function classify($qs){
-  // instantiate cUrl
-  
-  $url = curl_init($qs);
-  
-  // set options
-  curl_setopt($url, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($url, CURLOPT_HEADER, 0);
-  
-  // execute, close & return result
-  $data = curl_exec($url);
-  curl_close($url);
-  
-  return $data;
+class uClassify {
+	
+	// private properties
+	private $baseUrl = 'http://uclassify.com/browse/uClassify/';
+	private $readkey = 'vhSVEuFSAZXrYxkCj3oxXN0TI+E='; // <- your read key here
+	private $removeHTML = 1;
+	private $encoding = 'json';
+	private $classifier = "topics"; // default to topics command
+	
+	// Class methods
+    public function __construct()  {  
+    	//echo 'The class "', __CLASS__, '" was initiated!<br />';
+    }  
+	
+    public function __destruct() { 
+    	//echo 'The class "', __CLASS__, '" was destroyed.<br />';
+    }  
+    
+    public function __toString() {
+    	return implode(', ', get_object_vars($this));
+    }
+	
+    // Property accessors
+    
+    // set removeHTML
+	public function setRemoveHTML($trueOrFalse) {
+		if($trueOrFalse){
+			$this->removeHTML = 1;
+		} else {
+			$this->removeHTML = 0;
+		}
+		return $this->removeHTML;
+	}
+	
+	// get removeHTML
+	public function getRemoveHTML() {
+		return $this->removeHTML;
+	}
+	
+	// set encoding
+	public function setEncoding($newEncoding) {
+		if($newEncoding == 'xml' || $newEncoding == 'json'){
+			$this->removeHTML = $newEncoding;
+			return 0;
+		} else {
+			return -1;
+		}
+	}
+	
+	// get encoding
+	public function getEncoding() {
+		return $this->Encoding;
+	}
+	
+	// Public methods
+	public function classifyUrl($url) {
+		// assemble the query string
+		$qs = $this->baseUrl.ucfirst($this->classifier).'/ClassifyUrl/?readkey='.urlencode($this->readkey).
+				'&url='.urlencode($url).'&removeHtml='.$this->removeHTML.'&output='.$this->encoding;
+		
+		// instantiate cUrl
+		$curl = curl_init($qs);
+		
+		// set options
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HEADER, 0);
+		
+		// execute, close & return result
+		$data = curl_exec($curl);
+		
+		curl_close($curl);
+		
+		return $data;
+	}
 }
 ?>
